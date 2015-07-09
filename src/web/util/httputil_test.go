27c3da5d16b19f17b72122ccb3b1b205eaa621ab
zhou zhenhua <citysir@qq.com>
2015-07-09 15:37:26 +0800

    增加测试代码

---
+++ src/web/util/httputil_test.go
@@ -0,0 +1,12 @@
+package util
+
+import "testing"
+
+func TestHttpGet(t *testing.T) {
+	data, err := HttpGet("http://www.baidu.com")
+	if err != nil {
+		t.Error("test TestHttpGet failed")
+	} else {
+		t.Log("test TestHttpGet pass %s", string(data))
+	}
+}
