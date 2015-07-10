e674c760470a9541ae0727fa6a4e0f7d64154187
zhou zhenhua <citysir@qq.com>
2015-07-10 14:00:07 +0800

    优化代码

--- src/web/rpcapi/echoservice.go
+++ src/web/rpcapi/echoservice.go
@@ -9,6 +9,6 @@ type EchoServiceImpl struct {
 
 func (this *EchoServiceImpl) Echo(callTime int64, helloCode string) (r string, err error) {
 	fmt.Println("-->Echo:", callTime, helloCode)
-	r = fmt.Sprintf("%s Gopher", helloCode)
+	r = fmt.Sprintf("%s, Gopher", helloCode)
 	return
 }
